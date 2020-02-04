94557a7ce4fe697a92555c503b0672762c5cbdc4
tsux2000 <tsux2000@gmail.com>
2020-02-04 17:51:51 +0900

    Make robots text

--- project/urls.py
+++ project/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,6 +19,7 @@ urlpatterns = [
             'articles': sitemaps.ArticleSitemap,
         }
     },  name='sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
     path('markdownx/', include('markdownx.urls')),
     path('', include('blog.urls')),
 ]
